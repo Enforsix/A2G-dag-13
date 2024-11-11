4516fdfd57e53479b0839c9476dfa1b991580239
Enforsix <christian.gulkoder@gmail.com>
2024-11-11 10:37:51 +0100

    java dag 13

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+
+function person() {
+    console.log("Hallo?")
+
+    const userName = "Christian";
+    const userAge = "38";
+    let isOnline = true;
+
+    //userName = "Enforsix";
+    //userAge = 39;
+    isOnline = false
+
+console.log(userName, userAge, isOnline);
+}
+person();
+
+function game() {
+    console.log("Star Citizen");
+}
+
+game();
