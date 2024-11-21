a835249dac0f97eee52d20b5997eed93b1449721
Enforsix <christian.gulkoder@gmail.com>
2024-11-21 11:03:44 +0100

    oppgave 14

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
+const knightText = document.querySelector("h3");
+console.log(knightText)
 
-function person() {
-    console.log("Hallo?")
-
-    const userName = "Christian";
-    const userAge = "38";
-    let isOnline = true;
-
-    //userName = "Enforsix";
-    //userAge = 39;
-    isOnline = false
-
-console.log(userName, userAge, isOnline);
-}
-person();
-
-function game() {
-    console.log("Star Citizen");
-}
-
-game();
+const paragraph = document.createElement("P");
+paragraph.textContent = "Knights are legendary!";
+document.body.appendChild(paragraph);
