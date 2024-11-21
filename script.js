ec502a559ef609266af4d1b8bebe96ecd7ae8704
Enforsix <christian.gulkoder@gmail.com>
2024-11-21 11:05:09 +0100

    oppgave 14 levert

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ console.log(knightText)
 const paragraph = document.createElement("P");
 paragraph.textContent = "Knights are legendary!";
 document.body.appendChild(paragraph);
+console.log(paragraph)
+
